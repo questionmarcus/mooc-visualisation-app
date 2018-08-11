939f2b2bf09f9a16837b35ded620f6394014ccda
Marcus <marcus.h.lancaster@gmail.com>
2018-08-11 17:36:35 +0200

    Calendar View working

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -3,10 +3,13 @@ import React, {Component} from "react";
 class CalendarDay extends Component {
     render() {
         return (
-            <rect width="20" height="20"
+            <rect
+                fill={this.props.colour}
+                width={this.props.size}
+                height={this.props.size}
                 x={this.props.xPos}
                 y={this.props.yPos}>
-                <title>{this.props.day}</title>
+                <title>{this.props.day+"\n"+this.props.value+" Active Users"}</title>
             </rect>
         );
     }
--- src/containers/Calendar.js
+++ src/containers/Calendar.js
@@ -3,6 +3,9 @@ import getAPIData from "../utils/api_connect.js";
 import LoadingScreen from "../components/LoadingScreen.js";
 import CalendarDay from "../components/CalendarDay.js";
 import * as time from "d3-time";
+import {min,max,range} from "d3-array";
+import {scaleLinear, scaleBand} from "d3-scale";
+import {interpolateYlGn} from "d3-scale-chromatic";
 
 class CalendarView extends Component {
     constructor(props) {
@@ -35,17 +38,66 @@ class CalendarView extends Component {
 
     render() {
         if (this.state.api_data) {
-            console.log(this.state.api_data[0]);
-            console.log(time.timeMonday.count(time.timeYear(this.state.api_data[0].date),this.state.api_data[0].date));
+            const startYear = min(this.state.api_data.map(ob => ob.date.getFullYear()));
+            const endYear = max(this.state.api_data.map(ob => ob.date.getFullYear()));
+            const yearHeight = 200;
+            const yearArray = range(startYear, endYear+1);
+            const height = yearArray.length * yearHeight;
+            const width = 1000;
+            const cellGap = 3;
+            const cellSize = (width/52)-cellGap;
+            const cScale = scaleLinear().domain(
+                [0, max(this.state.api_data.map(ob => ob.value))]
+            );
+            const xScale = scaleLinear().domain([0,52]).range([40,width]);
+            const yearScale = scaleBand().domain(yearArray).range([0,height]).padding(0.3);
+            console.log(yearScale(2017));
+            const yScale = scaleLinear().domain([0,6]).range([0,yearScale.bandwidth()]);
             const rects = this.state.api_data.map(ob => {
                 return <CalendarDay
-                    xPos={time.timeMonday.count(time.timeYear(ob.date),ob.date)*15+3}
-                    yPos={((ob.date.getDay() + 6) % 7)*15+3}
+                    xPos={xScale(time.timeMonday.count(time.timeYear(ob.date),ob.date))}
+                    yPos={yScale((ob.date.getDay() + 6) % 7)+yearScale(ob.date.getFullYear())}
+                    size={cellSize}
+                    colour={interpolateYlGn(cScale(ob.value))}
+                    value={ob.value}
                     day={ob.date.toDateString()}/>;
             });
-            console.log(rects);
+            const days = (year) =>
+                ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"].map((day,ind) =>
+                    <text
+                        x="15"
+                        y={yScale(ind)+yearScale(year)+cellSize-cellGap}
+                        fontSize="12px">
+                        {day}
+                    </text>
+                );
+
+            const months = (year) =>
+                ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].map((mon,ind) => {let startMonth = new Date(Date.UTC(year,ind,1,0,0,0));
+                    return (
+                        <text
+                            x={xScale(time.timeMonday.count(time.timeYear(startMonth),startMonth))}
+                            y={yScale(0)-5+yearScale(startMonth.getFullYear())}
+                            fontSize="12px">
+                            {mon}
+                        </text>
+                    );});
+          
+            const dayLabels = yearArray.map(year => days(year));
+            const monthLabels = yearArray.map(year => months(year));
+            const yearLabels = yearArray.map(year => 
+                <text
+                    x={xScale(0)}
+                    y={yScale(0)-20+yearScale(year)}
+                    fontSize="16px"
+                >{year}</text>
+            );
+            console.log(months(2016));
             return (
-                <svg width="100%" viewBox="0 0 1000 1000">
+                <svg width="100%" viewBox={"0 0 "+width+" "+height}>
+                    {yearLabels}
+                    {monthLabels}
+                    {dayLabels}
                     {rects}
                 </svg>
             );
