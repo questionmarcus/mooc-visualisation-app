23f50438ff6fda94641665234d8b688c9dd89d5d
Marcus <marcus.h.lancaster@gmail.com>
2018-08-12 11:53:36 +0200

    added backwards sankey diagram and set api_connect to look in local host when react dev-server is running

--- src/containers/CoursePath.js
+++ src/containers/CoursePath.js
@@ -9,25 +9,31 @@ class CoursePath extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {api_data: null};
+        this.state = {api_data: null,reverse:false};
         this.getData = this.getData.bind(this);
+        this.reversePath = this.reversePath.bind(this);
     }
 
     componentDidMount() {
         this.getData();
     }
 
-    componentDidUpdate(prevProps) {
-        if (prevProps.year !== this.props.year) {
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.year !== this.props.year || prevState.reverse !== this.state.reverse) {
             this.getData();
         }
     }
 
     getData() {
         this.setState({api_data:null});
-        getAPIData("studypath/"+this.props.year).then(
+        const urlString = this.state.reverse ? "studypath/"+this.props.year+"/reverse" : "studypath/"+this.props.year;
+        getAPIData(urlString).then(
             json => this.setState(() => {return {api_data: json};})
         );
+    }
+
+    reversePath() {
+        this.setState({reverse: this.state.reverse ? false : true});
     }
 
     render() {
@@ -37,21 +43,26 @@ class CoursePath extends Component {
             const sank = sankey().nodeWidth(25).nodePadding(0).size([width,height]);
             const { nodes, links } = sank(this.state.api_data);
             return(
-                <svg width="100%" viewBox={"0 0 "+width+" "+height}>
-                    {links.map(
-                        link => <SankeyPath
-                            settings={link}
-                            pathGen={sankeyLinkHorizontal()}
-                            key={link.index} />
-                    )}
-                    {nodes.map(
-                        node => <SankeyNode
-                            settings={node}
-                            figWidth={width}
-                            figHeight={height}
-                            key={node.name}/>
-                    )}
-                </svg>
+                <React.Fragment>
+                    <button onClick={() => {this.reversePath();}}>
+                        {this.state.reverse ? "Show Forward Progess" : "Show \"Back-tracking\" Progess"}
+                    </button>
+                    <svg width="100%" viewBox={"0 0 "+width+" "+height}>
+                        {links.map(
+                            link => <SankeyPath
+                                settings={link}
+                                pathGen={sankeyLinkHorizontal()}
+                                key={link.index} />
+                        )}
+                        {nodes.map(
+                            node => <SankeyNode
+                                settings={node}
+                                figWidth={width}
+                                figHeight={height}
+                                key={node.name}/>
+                        )}
+                    </svg>
+                </React.Fragment>
             );
         } else {
             return (
--- src/utils/api_connect.js
+++ src/utils/api_connect.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const getAPIData = (urlSuffix) => {
-    let url = window.encodeURI("https://questionmarcus.pythonanywhere.com/"+urlSuffix);
+    const currentURL = window.location.href;
+    const baseURL = currentURL.includes("localhost") ? "http://localhost:5000/" : "https://questionmarcus.pythonanywhere.com/";
+    let url = window.encodeURI(baseURL+urlSuffix);
     return axios.get(url).then( response => response.data );
 };
 
